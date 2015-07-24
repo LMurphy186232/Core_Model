1fa8a21ce7d6be65ff964203b29485c4830567a0
Lora Murphy <murphyl@caryinstitute.org>
2015-07-24 11:13:16 -0500

    Add shifted lognormal size effect with different parameters for infected

---
+++ Behaviors/NCI/SizeEffectShiftedLogInf.cpp
@@ -0,0 +1,208 @@
+#include "SizeEffectShiftedLogInf.h"
+#include "TreePopulation.h"
+#include "BehaviorBase.h"
+#include "ParsingFunctions.h"
+#include <math.h>
+#include <sstream>
+
+//////////////////////////////////////////////////////////////////////////////
+// Constructor
+//////////////////////////////////////////////////////////////////////////////
+clSizeEffectShiftedLogInf::clSizeEffectShiftedLogInf() {
+  mp_iDataCodes = NULL;
+  mp_fXb = NULL;
+  mp_fX0 = NULL;
+  mp_fXp = NULL;
+  mp_fXbInf = NULL;
+  mp_fX0Inf = NULL;
+  mp_fXpInf = NULL;
+  mp_fMinDiam = NULL;
+  m_iNumSpecies = 0;
+}
+
+////////////////////////////////////////////////////////////////////////////
+// Destructor
+////////////////////////////////////////////////////////////////////////////
+clSizeEffectShiftedLogInf::~clSizeEffectShiftedLogInf() {
+  delete[] mp_fXb;
+  delete[] mp_fX0;
+  delete[] mp_fXp;
+
+  delete[] mp_fXbInf;
+  delete[] mp_fX0Inf;
+  delete[] mp_fXpInf;
+
+  delete[] mp_fMinDiam;
+
+  if ( mp_iDataCodes ) {
+    for ( int i = 0; i < m_iNumSpecies; i++ ) {
+      delete[] mp_iDataCodes[i];
+    }
+  }
+  delete[] mp_iDataCodes;
+}
+
+////////////////////////////////////////////////////////////////////////////
+// CalculateSizeEffect
+////////////////////////////////////////////////////////////////////////////
+float clSizeEffectShiftedLogInf::CalculateSizeEffect(clTree *p_oTree, const float &fDiam) {
+  float fSizeEffect = 0, fDiam2;
+  int iSpecies = p_oTree->GetSpecies(), iYrsInfested;
+
+  //Get the infestation status for this tree
+  p_oTree->GetValue( mp_iDataCodes[iSpecies][p_oTree->GetType()], &iYrsInfested);
+
+  //Make sure the diameter is above the minimum
+  fDiam2 = (fDiam < mp_fMinDiam[iSpecies]) ? mp_fMinDiam[iSpecies] : fDiam;
+
+  if (iYrsInfested == 0) { //not infested
+    fSizeEffect = exp(-0.5 * pow(log((fDiam2 + mp_fXp[iSpecies]) / mp_fX0[iSpecies] ) / mp_fXb[iSpecies], 2));
+  } else {
+    fSizeEffect = exp(-0.5 * pow(log((fDiam2 + mp_fXpInf[iSpecies]) / mp_fX0Inf[iSpecies] ) / mp_fXbInf[iSpecies], 2));
+  }
+  //Make sure it's bounded between 0 and 1
+  if ( fSizeEffect < 0 ) fSizeEffect = 0;
+  if ( fSizeEffect > 1 ) fSizeEffect = 1;
+  return fSizeEffect;
+}
+
+////////////////////////////////////////////////////////////////////////////
+// DoSetup
+////////////////////////////////////////////////////////////////////////////
+void clSizeEffectShiftedLogInf::DoSetup(clTreePopulation *p_oPop, clBehaviorBase *p_oNCI, xercesc::DOMElement *p_oElement) {
+  doubleVal * p_dTempValues; //for getting species-specific values
+  int iNumBehaviorSpecies = p_oNCI->GetNumBehaviorSpecies(),
+      iNumTypes = p_oPop->GetNumberOfTypes(), i, j;
+
+  m_iNumSpecies = p_oPop->GetNumberOfSpecies();
+
+  mp_fX0 = new double[m_iNumSpecies];
+  mp_fXb = new double[m_iNumSpecies];
+  mp_fXp = new double[m_iNumSpecies];
+  mp_fX0Inf = new double[m_iNumSpecies];
+  mp_fXbInf = new double[m_iNumSpecies];
+  mp_fXpInf = new double[m_iNumSpecies];
+  mp_fMinDiam = new double[m_iNumSpecies];
+
+  //Set up our floatVal array that will extract values only for the species
+  //assigned to this behavior
+  p_dTempValues = new doubleVal[iNumBehaviorSpecies];
+  for ( i = 0; i < iNumBehaviorSpecies; i++ ) p_dTempValues[i].code = p_oNCI->GetBehaviorSpecies(i);
+
+  //Size effect mode (X0)
+  FillSpeciesSpecificValue( p_oElement, "nciSizeEffectX0", "nsex0Val",
+      p_dTempValues, iNumBehaviorSpecies, p_oPop, true);
+  //Transfer to the appropriate array buckets
+  for ( i = 0; i < iNumBehaviorSpecies; i++ )
+    mp_fX0[p_dTempValues[i].code] = p_dTempValues[i].val;
+
+  //Size effect variance (Xb)
+  FillSpeciesSpecificValue(p_oElement, "nciSizeEffectXb", "nsexbVal",
+      p_dTempValues, iNumBehaviorSpecies, p_oPop, true);
+  //Transfer to the appropriate array buckets
+  for ( i = 0; i < iNumBehaviorSpecies; i++)
+    mp_fXb[p_dTempValues[i].code] = p_dTempValues[i].val;
+
+  //Size effect shift (Xp)
+  FillSpeciesSpecificValue(p_oElement, "nciSizeEffectXp", "nsexpVal",
+      p_dTempValues, iNumBehaviorSpecies, p_oPop, true);
+  //Transfer to the appropriate array buckets
+  for ( i = 0; i < iNumBehaviorSpecies; i++)
+    mp_fXp[p_dTempValues[i].code] = p_dTempValues[i].val;
+
+  //Size effect mode (X0) - infected
+  FillSpeciesSpecificValue( p_oElement, "nciSizeEffectInfX0", "nseix0Val",
+      p_dTempValues, iNumBehaviorSpecies, p_oPop, true);
+  //Transfer to the appropriate array buckets
+  for ( i = 0; i < iNumBehaviorSpecies; i++ )
+    mp_fX0Inf[p_dTempValues[i].code] = p_dTempValues[i].val;
+
+  //Size effect variance (Xb) - infected
+  FillSpeciesSpecificValue(p_oElement, "nciSizeEffectInfXb", "nseixbVal",
+      p_dTempValues, iNumBehaviorSpecies, p_oPop, true);
+  //Transfer to the appropriate array buckets
+  for ( i = 0; i < iNumBehaviorSpecies; i++)
+    mp_fXbInf[p_dTempValues[i].code] = p_dTempValues[i].val;
+
+  //Size effect shift (Xp) - infected
+  FillSpeciesSpecificValue(p_oElement, "nciSizeEffectInfXp", "nseixpVal",
+      p_dTempValues, iNumBehaviorSpecies, p_oPop, true);
+  //Transfer to the appropriate array buckets
+  for ( i = 0; i < iNumBehaviorSpecies; i++)
+    mp_fXpInf[p_dTempValues[i].code] = p_dTempValues[i].val;
+
+  //Size effect lower bound
+  FillSpeciesSpecificValue(p_oElement, "nciSizeEffectLowerBound", "nselbVal",
+      p_dTempValues, iNumBehaviorSpecies, p_oPop, true);
+  //Transfer to the appropriate array buckets
+  for ( i = 0; i < iNumBehaviorSpecies; i++)
+    mp_fMinDiam[p_dTempValues[i].code] = p_dTempValues[i].val;
+
+  delete[] p_dTempValues;
+
+  //Make sure that the size effect mode is not 0
+  for ( i = 0; i < iNumBehaviorSpecies; i++) {
+    if (0 >= mp_fX0[p_oNCI->GetBehaviorSpecies(i)]) {
+      modelErr stcErr;
+      stcErr.iErrorCode = BAD_DATA;
+      stcErr.sFunction = "clSizeEffectShiftedLogInf::DoSetup";
+      stcErr.sMoreInfo = "NCI size effect mode (X0) values must be greater than 0.";
+      throw( stcErr );
+    }
+
+    if (0 >= mp_fX0Inf[p_oNCI->GetBehaviorSpecies(i)]) {
+      modelErr stcErr;
+      stcErr.iErrorCode = BAD_DATA;
+      stcErr.sFunction = "clSizeEffectShiftedLogInf::DoSetup";
+      stcErr.sMoreInfo = "NCI size effect mode (X0) values must be greater than 0.";
+      throw( stcErr );
+    }
+
+    //Make sure that the size effect variance is not 0
+    if (0 == mp_fXb[p_oNCI->GetBehaviorSpecies(i)]) {
+      modelErr stcErr;
+      stcErr.iErrorCode = BAD_DATA;
+      stcErr.sFunction = "clSizeEffectShiftedLogInf::DoSetup";
+      stcErr.sMoreInfo = "NCI size effect variance (Xb) values cannot be 0.";
+      throw( stcErr );
+    }
+
+    if (0 == mp_fXbInf[p_oNCI->GetBehaviorSpecies(i)]) {
+      modelErr stcErr;
+      stcErr.iErrorCode = BAD_DATA;
+      stcErr.sFunction = "clSizeEffectShiftedLogInf::DoSetup";
+      stcErr.sMoreInfo = "NCI size effect variance (Xb) values cannot be 0.";
+      throw( stcErr );
+    }
+  }
+
+  //Collect the "YearsInfested" codes
+  mp_iDataCodes = new short int *[m_iNumSpecies];
+  for (i = 0; i < m_iNumSpecies; i++) {
+    mp_iDataCodes[i] = new short int[iNumTypes];
+    for (j = 0; j < iNumTypes; j++) {
+      mp_iDataCodes[i][j] = -1;
+    }
+  }
+  for (i = 0; i < p_oNCI->GetNumSpeciesTypeCombos(); i++ )
+  {
+    mp_iDataCodes[p_oNCI->GetSpeciesTypeCombo(i).iSpecies]
+                 [p_oNCI->GetSpeciesTypeCombo(i).iType] =
+                      p_oPop->GetIntDataCode("YearsInfested",
+                          p_oNCI->GetSpeciesTypeCombo(i).iSpecies,
+                          p_oNCI->GetSpeciesTypeCombo(i).iType );
+    if (-1 == mp_iDataCodes[p_oNCI->GetSpeciesTypeCombo(i).iSpecies]
+                           [p_oNCI->GetSpeciesTypeCombo(i).iType] )
+    {
+      modelErr stcErr;
+      stcErr.sFunction = "clSizeEffectShiftedLogInf::DoSetup" ;
+      std::stringstream s;
+      s << "Type/species combo species="
+        << p_oNCI->GetSpeciesTypeCombo(i).iSpecies << " type="
+        << p_oNCI->GetSpeciesTypeCombo(i).iType
+        << " does not have the insect infestation behavior.";
+      stcErr.iErrorCode = BAD_DATA;
+      throw( stcErr );
+    }
+  }
+}
---
+++ Behaviors/NCI/SizeEffectShiftedLogInf.h
@@ -0,0 +1,94 @@
+#ifndef SIZEEFFECTSHIFTEDLOGINF_H_
+#define SIZEEFFECTSHIFTEDLOGINF_H_
+
+#include "SizeEffectBase.h"
+
+/**
+ * Calculates a size effect based on a shifted lognormal function. This uses
+ * two different sets of parameters, depending on whether a tree is infected
+ * or not.
+ *
+ * Size Effect is calculated as:
+ * <center><i>SE = exp(-0.5(ln((diam+X<sub>p</sub>)/X<sub>0</sub>)/X<sub>b</sub>)<sup>2</sup>)</i></center>
+ *
+ * where:
+ * <ul>
+ * <li><i>diam</i> is the diameter of the target tree, in cm (d10 for seedlings, DBH for everyone else)</li>
+ * <li><i>X<sub>0</sub></i> is the size effect mode, in cm</li>
+ * <li><i>X<sub>b</sub></i> is the size effect variance, in cm</li>
+ * <li><i>X<sub>p</sub></i> is the size effect shift, in cm</li>
+ * </ul>
+ *
+ * Insect infestation is determined by the behavior of class
+ * clInsectInfestation or clDensDepInfestation. This class expects to find a
+ * "YearsInfested" int data member to be present for all trees to which this
+ * behavior is applied.
+ */
+class clSizeEffectShiftedLogInf: virtual public clSizeEffectBase {
+public:
+
+  /**
+   * Constructor.
+   */
+  clSizeEffectShiftedLogInf();
+
+  /**
+   * Destructor.
+   */
+  ~clSizeEffectShiftedLogInf();
+
+  /**
+   * Calculates size effect. This is bounded between 0 and 1.
+   @param p_oTree Tree for which to calculate size effect.
+   * @param fDiam Diameter of tree. Diameter at 10 cm for seedlings, DBH for
+   * all other types.
+   */
+  float CalculateSizeEffect(clTree *p_oTree, const float &fDiam);
+
+  /**
+   * Does any desired setup.
+   * @param p_oPop Tree population.
+   * @param p_oNCI NCI behavior object.
+   * @param p_oElement Root element of the behavior.
+   * @throws ModelException if X0 is < 0 or Xb = 0.
+   */
+  void DoSetup(clTreePopulation *p_oPop, clBehaviorBase *p_oNCI, xercesc::DOMElement *p_oElement);
+
+protected:
+
+  /** Codes for "YearsInfested" data member. Array size is number of species by
+   * number of types.*/
+  short int **mp_iDataCodes;
+
+  /**Size effect variance parameter for uninfested trees. X<sub>b</sub> in Size
+   * Effect equation above. Array is sized number of species.*/
+  double *mp_fXb;
+
+  /**Size effect mode parameter for uninfested trees. X<sub>0</sub> in Size
+   * Effect equation above. Array is sized number of species.*/
+  double *mp_fX0;
+
+  /**Size effect shift parameter for uninfested trees. X<sub>p</sub> in Size
+   * Effect equation above. Array is sized number of species.*/
+  double *mp_fXp;
+
+  /**Size effect variance parameter for infested trees. X<sub>b</sub> in Size
+   * Effect equation above. Array is sized number of species.*/
+  double *mp_fXbInf;
+
+  /**Size effect mode parameter for infested trees. X<sub>0</sub> in Size
+   * Effect equation above. Array is sized number of species.*/
+  double *mp_fX0Inf;
+
+  /**Size effect shift parameter for infested trees. X<sub>p</sub> in Size
+   * Effect equation above. Array is sized number of species.*/
+  double *mp_fXpInf;
+
+  /**Size effect minimum diameter. Array is sized number of species.*/
+  double *mp_fMinDiam;
+
+  /** Number of species. For destructor.*/
+  short int m_iNumSpecies;
+};
+
+#endif /* SIZEEFFECTSHIFTEDLOGINF_H_ */
