8239a1ea648024f7f9bda3c9da4be5d3e19c2ccb
Lora Murphy <murphyl@caryinstitute.org>
2015-07-24 11:07:55 -0500

    Add size dependent logistic mortality

---
+++ Behaviors/SizeDependentLogisticMortality.cpp
@@ -0,0 +1,124 @@
+//---------------------------------------------------------------------------
+#include "SizeDependentLogisticMortality.h"
+//---------------------------------------------------------------------------
+#include "MortalityOrg.h"
+#include "ParsingFunctions.h"
+#include "TreePopulation.h"
+#include "SimManager.h"
+#include "ModelMath.h"
+#include <math.h>
+
+////////////////////////////////////////////////////////////////////////////
+// Constructor
+////////////////////////////////////////////////////////////////////////////
+clSizeDependentLogisticMortality::clSizeDependentLogisticMortality(clSimManager *p_oSimManager) :
+clWorkerBase( p_oSimManager ), clBehaviorBase( p_oSimManager ),
+clMortalityBase( p_oSimManager ) {
+  try {
+
+    m_sNameString = "sizedeplogisticmortshell";
+    m_sXMLRoot = "SizeDependentLogisticMortality";
+
+    mp_fMax = NULL;
+    mp_fX0 = NULL;
+    mp_fXb = NULL;
+
+    m_fNumberYearsPerTimestep = 0;
+  }
+  catch (modelErr&err) {throw(err);}
+  catch (modelMsg &msg) {throw(msg);} //non-fatal error
+  catch (...) {
+    modelErr stcErr;
+    stcErr.iErrorCode = UNKNOWN;
+    stcErr.sFunction = "clSizeDependentLogisticMortality::clSizeDependentLogisticMortality";
+    throw(stcErr);
+  }
+}
+
+////////////////////////////////////////////////////////////////////////////
+// Destructor
+////////////////////////////////////////////////////////////////////////////
+clSizeDependentLogisticMortality::~clSizeDependentLogisticMortality() {
+  delete[] mp_fMax;
+  delete[] mp_fX0;
+  delete[] mp_fXb;
+}
+
+////////////////////////////////////////////////////////////////////////////
+// DoShellSetup()
+////////////////////////////////////////////////////////////////////////////
+void clSizeDependentLogisticMortality::DoShellSetup(xercesc::DOMDocument *p_oDoc) {
+  try {
+    clTreePopulation *p_oPop = (clTreePopulation*) mp_oSimManager->GetPopulationObject("treepopulation");
+    DOMElement *p_oElement = GetParentParametersElement(p_oDoc);
+    floatVal *p_fTempValues;  //for getting species-specific values
+    short int iNumSpecies = mp_oMortalityOrg->GetNumberOfSpecies(),
+        i; //loop counter
+
+    m_fNumberYearsPerTimestep = mp_oSimManager->GetNumberOfYearsPerTimestep();
+
+    //Declare the temp array and populate it with the species to which this
+    //behavior applies
+    p_fTempValues = new floatVal[m_iNumBehaviorSpecies];
+    for (i = 0; i < m_iNumBehaviorSpecies; i++)
+      p_fTempValues[i].code = mp_iWhatSpecies[i];
+
+    //Declare the arrays for holding the variables
+    mp_fMax = new float[iNumSpecies];
+    mp_fX0 = new float[iNumSpecies];
+    mp_fXb = new float[iNumSpecies];
+
+    //Capture the values from the parameter file
+
+    //Max
+    FillSpeciesSpecificValue(p_oElement, "mo_sizeDepLogMax", "mo_sdlmVal",
+        p_fTempValues, m_iNumBehaviorSpecies, p_oPop, true);
+    //Transfer to the appropriate array buckets
+    for (i = 0; i < m_iNumBehaviorSpecies; i++)
+      mp_fMax[p_fTempValues[i].code] = p_fTempValues[i].val;
+
+    //X0
+    FillSpeciesSpecificValue(p_oElement, "mo_sizeDepLogX0", "mo_sdlx0Val",
+        p_fTempValues, m_iNumBehaviorSpecies, p_oPop, true);
+    //Transfer to the appropriate array buckets
+    for (i = 0; i < m_iNumBehaviorSpecies; i++)
+      mp_fX0[p_fTempValues[i].code] = p_fTempValues[i].val;
+
+    //Xb
+    FillSpeciesSpecificValue(p_oElement, "mo_sizeDepLogXb", "mo_sdlxbVal",
+        p_fTempValues, m_iNumBehaviorSpecies, p_oPop, true);
+    //Transfer to the appropriate array buckets
+    for (i = 0; i < m_iNumBehaviorSpecies; i++)
+      mp_fXb[p_fTempValues[i].code] = p_fTempValues[i].val;
+
+    delete[] p_fTempValues;
+  }
+  catch (modelErr&err) {throw(err);}
+  catch (modelMsg &msg) {throw(msg);} //non-fatal error
+  catch (...) {
+    modelErr stcErr;
+    stcErr.iErrorCode = UNKNOWN;
+    stcErr.sFunction = "clSizeDependentLogisticMortality::DoShellSetup";
+    throw(stcErr);
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////
+// DoMort()
+////////////////////////////////////////////////////////////////////////////
+deadCode clSizeDependentLogisticMortality::DoMort(clTree *p_oTree, const float &fDiam, const short int &iSpecies) {
+  float fRandom = clModelMath::GetRand(), //random number
+        fAnnualDeathProb,    //annual probability of death
+        fTimestepDeathProb;  //death probability for the entire timestep
+
+  fAnnualDeathProb = mp_fMax[iSpecies]/(1+pow(fDiam/mp_fX0[iSpecies], mp_fXb[iSpecies]));
+
+  //Compound into the death probability for the whole timestep
+  fTimestepDeathProb = 1 -
+      pow(1 - fAnnualDeathProb, m_fNumberYearsPerTimestep);
+
+  if (fRandom < fTimestepDeathProb)
+    return natural;
+  else
+    return notdead;
+}
---
+++ Behaviors/SizeDependentLogisticMortality.h
@@ -0,0 +1,75 @@
+//---------------------------------------------------------------------------
+
+#ifndef SizeDependentLogisticMortalityH
+#define SizeDependentLogisticMortalityH
+//---------------------------------------------------------------------------
+#include "MortalityBase.h"
+/**
+ * Size Dependent Logistic Mortality - Version 1.0
+ *
+ * This evaluates mortality according to a logistic function of size.
+ *
+ * Probability of mortality is:
+ *
+ * P = MAX/(1+(diam/X0)^Xb))
+ *
+ * This class's namestring is "sizedeplogisticmortshell". The parameter file
+ * call string is "SizeDependentLogisticMortality".
+ *
+ * Copyright 2014 Charles D. Canham.
+ * @author Lora E. Murphy
+ *
+ * <br>Edit history:
+ * <br>-----------------
+ * <br>April 8, 2014 - Wiped the slate clean for SORTIE 7.0 (LEM)
+ */
+
+class clSizeDependentLogisticMortality : virtual public clMortalityBase {
+  //note: need the virtual keyword to avoid base class ambiguity.
+
+public:
+
+  /**
+   * Constructor. Sets the namestring and NULLs pointers.
+   * @param p_oSimManager Sim Manager object.
+   */
+  clSizeDependentLogisticMortality(clSimManager *p_oSimManager);
+
+  /**
+   * Destructor. Deletes arrays.
+   */
+  ~clSizeDependentLogisticMortality();
+
+  /**
+   * Reads in values from the parameter file.
+   * @param p_oDoc DOM tree of parsed input file.
+   */
+  void DoShellSetup(xercesc::DOMDocument *p_oDoc);
+
+  /**
+   * Calculates mortality according to the size dependent logistic mortality
+   * function.
+   *
+   * @param p_oTree Tree being evaluated
+   * @param fDiam DBH of tree being evaluated - for seedlings will be 0
+   * @param iSpecies Species of the tree being evaluated
+   * @return natural if the tree is to die, notdead if it lives.
+   */
+  deadCode DoMort(clTree *p_oTree, const float &fDiam, const short int &iSpecies);
+
+protected:
+
+  /**Max parameter. Array size # species */
+  float *mp_fMax;
+
+  /**X0 parameter. Array size # species */
+  float *mp_fX0;
+
+  /**Xb parameter. Array size # species */
+  float *mp_fXb;
+
+  /**Number years/timestep*/
+  float m_fNumberYearsPerTimestep;
+};
+//---------------------------------------------------------------------------
+#endif
