dd5bc5e771d373bcbcc0db7eb6fd4afd61078a47
Lora Murphy <murphyl@caryinstitute.org>
2015-07-24 11:20:39 -0500

    Add BA Ratio with NCI to NCI choides

---
+++ Behaviors/NCI/NCITermNCIBARatio.cpp
@@ -0,0 +1,264 @@
+#include "NCITermNCIBARatio.h"
+#include "TreePopulation.h"
+#include "Plot.h"
+#include "SimManager.h"
+#include "BehaviorBase.h"
+#include "ParsingFunctions.h"
+#include <sstream>
+#include <math.h>
+
+//////////////////////////////////////////////////////////////////////////////
+// Constructor
+//////////////////////////////////////////////////////////////////////////////
+clNCITermNCIBARatio::clNCITermNCIBARatio(bool bUseDefaultBA) {
+  mp_fAlpha = NULL;
+  mp_fBeta = NULL;
+  mp_fLambda = NULL;
+  mp_fMinimumNeighborDBH = NULL;
+  mp_iWhatSpecies = NULL;
+
+  mp_oPlot = NULL;
+
+  m_fMaxCrowdingRadius = 0;
+  m_fMaxAdultRadius = 0;
+  m_fMaxSaplingRadius = 0;
+  m_iNumTotalSpecies = 0;
+  m_iNumBehaviorSpecies = 0;
+  m_fMinSaplingHeight = 0;
+  m_fDbhAdjustor = 1;
+
+  m_bUseDefaultBA = bUseDefaultBA;
+  m_fDefaultBA = 0;
+  iNumNCIs = 2;
+  bRequiresTargetDiam = !m_bUseDefaultBA;
+
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// Destructor
+//////////////////////////////////////////////////////////////////////////////
+clNCITermNCIBARatio::~clNCITermNCIBARatio() {
+  int i;
+
+  delete[] mp_fAlpha;
+  delete[] mp_fBeta;
+  delete[] mp_iWhatSpecies;
+
+  if (mp_fLambda)
+    for (i = 0; i < m_iNumTotalSpecies; i++)
+      delete[] mp_fLambda[i];
+  delete[] mp_fLambda;
+
+  delete[] mp_fMinimumNeighborDBH;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// CalculateNCITerm
+//////////////////////////////////////////////////////////////////////////////
+clNCITermBase::ncivals clNCITermNCIBARatio::CalculateNCITerm(clTree * p_oTree, clTreePopulation * p_oPop, clPlot * p_oPlot, const float &fX, const float &fY, const int &iSpecies) {
+
+  clTreeSearch * p_oAllNeighbors; //neighborhood trees within crowding radius
+  clTree * p_oNeighbor; //competing neighbor
+  ncivals toReturn;
+  std::stringstream sQuery; //format search strings into this
+  float fNCI = 0, //nci - the end result of all this math
+      fDistance, //distance between target and neighbor
+      fDbh,
+      fTotBA = 0,
+      fBARatio,
+      fNeighBA,
+      fNeighDbh,
+      fNeighX, fNeighY; //holders for the neighbor tree's X and Y
+  int iIsDead,
+      iNumNeighbors = 0;
+  short int iNeighSpecies, iNeighType, //species and type for neighbor
+  iDeadCode; //neighbor's dead code
+
+  if (!m_bUseDefaultBA) p_oTree->GetValue( p_oPop->GetDbhCode( p_oTree->GetSpecies(), p_oTree->GetType() ), & fDbh );
+
+  //Format the query to get all competing neighbors
+
+  //Get all trees taller than seedlings within the max crowding radius -
+  //seedlings don't compete
+  sQuery << "distance=" << m_fMaxCrowdingRadius << "FROM x="
+         << fX << "y=" << fY << "::height=" << m_fMinSaplingHeight;
+  p_oAllNeighbors = p_oPop->Find(sQuery.str());
+
+  //Loop through and assess the competitive effects of each
+  p_oNeighbor = p_oAllNeighbors->NextTree();
+
+  while ( p_oNeighbor ) {
+    if ( p_oNeighbor == p_oTree ) goto nextTree;
+
+    iNeighSpecies = p_oNeighbor->GetSpecies();
+    iNeighType = p_oNeighbor->GetType();
+
+    if ( clTreePopulation::seedling == iNeighType ||
+        clTreePopulation::snag == iNeighType) goto nextTree;
+
+    //Make sure the neighbor's not dead
+    iDeadCode = p_oPop->GetIntDataCode( "dead", iNeighSpecies, iNeighType );
+    if ( -1 != iDeadCode )  p_oNeighbor->GetValue( iDeadCode, & iIsDead );
+    else iIsDead = notdead;
+
+    if (iIsDead > natural) goto nextTree;
+
+    //Get the neighbor's dbh
+    p_oNeighbor->GetValue( p_oPop->GetDbhCode( iNeighSpecies, iNeighType ), & fNeighDbh );
+
+    if ( fNeighDbh < mp_fMinimumNeighborDBH[iNeighSpecies] ) goto nextTree;
+
+    //Get the neighbor's X and Y values
+    p_oNeighbor->GetValue( p_oPop->GetXCode( iNeighSpecies, iNeighType ), & fNeighX );
+    p_oNeighbor->GetValue( p_oPop->GetYCode( iNeighSpecies, iNeighType ), & fNeighY );
+
+    //Get the distance between the two trees
+    fDistance = p_oPlot->GetDistance( fX, fY, fNeighX, fNeighY );
+
+    //Make sure it's within the appropriate radius
+    if ((m_fMaxAdultRadius < fDistance && clTreePopulation::adult == iNeighType) ||
+        (m_fMaxSaplingRadius < fDistance && clTreePopulation::sapling == iNeighType) ||
+        0 == fDistance)
+      goto nextTree;
+
+    fNeighBA = clModelMath::CalculateBasalArea(fNeighDbh);
+    fTotBA += fNeighBA;
+    iNumNeighbors++;
+
+    //Add competitive effect to NCI
+    fNCI += mp_fLambda[iSpecies][iNeighSpecies] *
+        (pow( (fNeighDbh * m_fDbhAdjustor), mp_fAlpha[iSpecies])
+            / pow( fDistance, mp_fBeta[iSpecies] ) );
+
+
+    nextTree:
+    p_oNeighbor = p_oAllNeighbors->NextTree();
+  }
+
+  //Calculate BA ratio
+  if (fTotBA > 0) {
+    if (m_bUseDefaultBA) {
+      fBARatio = (fTotBA / iNumNeighbors) / m_fDefaultBA;
+    } else {
+      fBARatio = (fTotBA / iNumNeighbors) / clModelMath::CalculateBasalArea(fDbh);
+    }
+  }
+  else fBARatio = 1;
+
+  toReturn.fNCI1 = fBARatio;
+  toReturn.fNCI2 = fNCI;
+  return toReturn;
+}
+
+//////////////////////////////////////////////////////////////////////////////
+// DoSetup
+//////////////////////////////////////////////////////////////////////////////
+void clNCITermNCIBARatio::DoSetup(clTreePopulation *p_oPop, clBehaviorBase *p_oNCI, xercesc::DOMElement *p_oElement) {
+  floatVal * p_fTempValues; //for getting species-specific values
+  std::stringstream sLabel;
+  int i, j;
+
+  m_iNumBehaviorSpecies = p_oNCI->GetNumBehaviorSpecies();
+  mp_iWhatSpecies = new short int[m_iNumBehaviorSpecies];
+  for (i = 0; i < m_iNumBehaviorSpecies; i++) {
+    mp_iWhatSpecies[i] = p_oNCI->GetBehaviorSpecies(i);
+  }
+
+  //Get a pointer to the plot object
+  mp_oPlot = p_oNCI->GetSimManager()->GetPlotObject();
+
+  m_iNumTotalSpecies = p_oPop->GetNumberOfSpecies();
+  m_fMinSaplingHeight = 50;
+  //Get the minimum sapling height
+  for (i = 0; i < m_iNumTotalSpecies; i++) {
+    if ( p_oPop->GetMaxSeedlingHeight( i ) < m_fMinSaplingHeight) {
+      m_fMinSaplingHeight = p_oPop->GetMaxSeedlingHeight( i );
+    }
+  }
+
+  mp_fAlpha = new float[m_iNumTotalSpecies];
+  mp_fBeta = new float[m_iNumTotalSpecies];
+  mp_fMinimumNeighborDBH = new float[m_iNumTotalSpecies];
+  mp_fLambda = new float*[m_iNumTotalSpecies];
+  for (i = 0; i < m_iNumTotalSpecies; i++) {
+    mp_fLambda[i] = new float[m_iNumTotalSpecies];
+    for (j = 0; j < m_iNumTotalSpecies; j++) {
+      mp_fLambda[i][j] = -1;
+    }
+  }
+
+  //Set up our floatVal array that will extract values only for the species
+  //assigned to this behavior
+  p_fTempValues = new floatVal[m_iNumBehaviorSpecies];
+  for ( i = 0; i < m_iNumBehaviorSpecies; i++ )
+    p_fTempValues[i].code = p_oNCI->GetBehaviorSpecies(i);
+
+  //Max adult crowding radius
+  FillSingleValue( p_oElement, "nciMaxAdultCrowdingRadius", &m_fMaxAdultRadius, true );
+
+  //Max sapling crowding radius
+  FillSingleValue( p_oElement, "nciMaxSaplingCrowdingRadius", &m_fMaxSaplingRadius, true);
+
+  m_fMaxCrowdingRadius = m_fMaxAdultRadius > m_fMaxSaplingRadius ? m_fMaxAdultRadius : m_fMaxSaplingRadius;
+
+  //NCI DBH adjustor
+  FillSingleValue(p_oElement, "nciDbhAdjustor", & m_fDbhAdjustor, true);
+
+  if (m_bUseDefaultBA) {
+    //Get the default DBH
+    FillSingleValue(p_oElement, "nciBADefaultDBH", &m_fDefaultBA, true);
+    //Calculate the BA
+    m_fDefaultBA = clModelMath::CalculateBasalArea(m_fDefaultBA);
+  }
+
+  //Neighbor DBH effect (alpha)
+  FillSpeciesSpecificValue(p_oElement, "nciAlpha", "naVal", p_fTempValues,
+      m_iNumBehaviorSpecies, p_oPop, true);
+  //Transfer to the appropriate array buckets
+  for ( i = 0; i < m_iNumBehaviorSpecies; i++ )
+    mp_fAlpha[p_fTempValues[i].code] = p_fTempValues[i].val;
+
+  //Neighbor distance effect (beta)
+  FillSpeciesSpecificValue(p_oElement, "nciBeta", "nbVal", p_fTempValues,
+      m_iNumBehaviorSpecies, p_oPop, true );
+  //Transfer to the appropriate array buckets
+  for (i = 0; i < m_iNumBehaviorSpecies; i++ )
+    mp_fBeta[p_fTempValues[i].code] = p_fTempValues[i].val;
+
+  //Lambda
+  for (i = 0; i < m_iNumTotalSpecies; i++) {
+    sLabel << "nci" << p_oPop->TranslateSpeciesCodeToName(i) << "NeighborLambda";
+    FillSpeciesSpecificValue(p_oElement, sLabel.str(), "nlVal",
+        p_fTempValues, m_iNumBehaviorSpecies, p_oPop, true);
+    sLabel.str("");
+    for ( j = 0; j < m_iNumBehaviorSpecies; j++ )      {
+      mp_fLambda[p_fTempValues[j].code][i] = p_fTempValues[j].val;
+    }
+  }
+
+  //Minimum neighbor DBH
+  FillSpeciesSpecificValue( p_oElement, "nciMinNeighborDBH", "nmndVal",
+      mp_fMinimumNeighborDBH, p_oPop, true);
+
+  delete[] p_fTempValues;
+
+  //Make sure that the max radius of neighbor effects is > 0
+  if (m_fMaxCrowdingRadius < 0) {
+    modelErr stcErr;
+    stcErr.iErrorCode = BAD_DATA;
+    stcErr.sFunction = "clNCITermNCIBARatio::DoSetup";
+    stcErr.sMoreInfo = "All values for NCI max crowding radius must be greater than 0.";
+    throw( stcErr );
+  }
+
+  for (i = 0; i < m_iNumTotalSpecies; i++) {
+    //Make sure that the minimum neighbor DBH is not negative
+    if (0 > mp_fMinimumNeighborDBH[i]) {
+      modelErr stcErr;
+      stcErr.iErrorCode = BAD_DATA;
+      stcErr.sFunction = "clNCITermNCIBARatio::DoSetup";
+      stcErr.sMoreInfo = "Minimum neighbor DBH for NCI cannot be less than 0.";
+      throw( stcErr );
+    }
+  }
+}
---
+++ Behaviors/NCI/NCITermNCIBARatio.h
@@ -0,0 +1,128 @@
+#ifndef NCITERMNCIBARATIO_H_
+#define NCITERMNCIBARATIO_H_
+
+#include "NCITermBase.h"
+
+/**
+ * This calculates NCI for a tree according to the function:
+ *
+ * @htmlonly
+  <center><i>NCI<sub>i</sub> = &Sigma; &lambda;<sub>k</sub>((DBH<sub>k</sub>*q)<sup>&alpha;</sup>/distance<sup>&beta;</sup>)</i></center>
+
+  where:
+  <ul>
+  <li>we're summing over k = 0 to N neighbors greater than the minimum
+  neighbor DBH</li>
+  <li><i>&alpha;</i> is the neighbor DBH effect parameter</li>
+  <li><i>&beta;</i> is the neighbor distance effect parameter</li>
+  <li><i>DBH<sub>k</sub></i> is the DBH of the kth neighbor, in cm</li>
+  <li><i>q</i> is the DBH units adjustment factor</li>
+  <li><i>&gamma;</i> is the size sensitivity to NCI parameter</li>
+  <li><i>&lambda;<sub>k</sub></i> is the NCI lambda parameter for the species
+  of the kth neighbor</li>
+  <li><i>distance</i> is distance from target to neighbor, in meters</li>
+  </ul>
+  <br>
+  @endhtmlonly
+ *
+ * NCI ignores neighbors with disturbance and harvest death codes. Natural deaths
+ * are NOT ignored, because it presumes that those deaths occurred in the current
+ * timestep and they should still be considered as live neighbors.
+ *
+ * This returns two terms: term #1 is the ratio of mean basal area of neighbors
+ * to target basal area; term #2 is the NCI.
+ *
+ * This behavior uses two different distances to look for neighbors. One is for
+ * saplings, and one is for adults.
+ */
+class clNCITermNCIBARatio: virtual public clNCITermBase {
+public:
+
+  /**
+   * Constructor.
+   */
+  clNCITermNCIBARatio(bool bUseDefaultBA);
+
+  /**
+   * Destructor.
+   */
+  ~clNCITermNCIBARatio();
+
+  /**
+   * Calculates NCI according to above equation.
+   * @param p_oTree Tree for which to calculate NCI.
+   * @param p_oPop Tree population.
+   * @param p_oPlot Plot object.
+   * @param fX X coordinate for which to calculate NCI.
+   * @param fY Y coordinate for which to calculate NCI.
+   * @param iSpecies Species for which to calculate NCI.
+   */
+  ncivals CalculateNCITerm(clTree * p_oTree, clTreePopulation * p_oPop, clPlot * p_oPlot, const float &fX, const float &fY, const int &iSpecies);
+
+  /**
+   * Does setup.
+   * @param p_oPop Tree population.
+   * @param p_oNCI NCI behavior object.
+   * @param p_oElement Root element of the behavior.
+   * @throws ModelException if the max radius of neighbor effects is < 0, or
+   * if DBH divisor is <= 0.
+   */
+  void DoSetup(clTreePopulation *p_oPop, clBehaviorBase *p_oNCI, xercesc::DOMElement *p_oElement);
+
+protected:
+
+  /**Pointer to the plot object, so we can get the temperature*/
+  clPlot *mp_oPlot;
+
+  /**Lambas. Array sized number of total species by number of total
+   * species.*/
+  float **mp_fLambda;
+
+  /**Neighbor DBH effect. @htmlonly &alpha; @endhtmlonly variable in
+   * equation above. Array sized number of species.*/
+  float *mp_fAlpha;
+
+  /**Neighbor distance effect. @htmlonly &beta; @endhtmlonly variable in
+   * equation above. Array sized number of species.*/
+  float *mp_fBeta;
+
+  /**The minimum DBH, in cm, of neighbors to be included in NCI calculations.
+   * Array assumed to be sized total number of species.*/
+  float *mp_fMinimumNeighborDBH;
+
+  /**List of distinct species.*/
+  short int *mp_iWhatSpecies;
+
+  /**Maximum search radius, in meters, in which to look for
+   * neighbors.*/
+  float m_fMaxCrowdingRadius;
+
+  /**Maximum search radius, in meters, in which to look for crowding adult
+   * neighbors.*/
+  float m_fMaxAdultRadius;
+
+  /**Maximum search radius, in meters, in which to look for crowding sapling
+   * neighbors.*/
+  float m_fMaxSaplingRadius;
+
+  /**The units adjustment factor for DBH in NCI. <i>q</i> in the NCI equation
+   * above. May be set to 1.*/
+  float m_fDbhAdjustor;
+
+  /**Minimum sapling height.  For doing neighbor searches.*/
+  float m_fMinSaplingHeight;
+
+  /**Basal area for default size, if m_bUseDefaultBA = true.*/
+  float m_fDefaultBA;
+
+  /**Number of behavior species.*/
+  int m_iNumBehaviorSpecies;
+
+  /**Number of total species. For the destructor.*/
+  int m_iNumTotalSpecies;
+
+  /**Whether or not to use a default BA, as with quadrat calcs*/
+  bool m_bUseDefaultBA;
+};
+
+#endif /* NCITERMNCIBARATIO_H_ */
